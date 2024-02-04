47d99f3120ec21f55c58aefecdf76eab5aa08113
AkifiNail <nail.akifi1234@gmail.com>
2024-02-04 22:53:41 +0100

    Welcome page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-import './_scroller.js';
+import "./_scroller.js";
 
-import './_chapter-1.js';
-import './_chapter-2.js';
-import './_chapter-3.js';
+import "./_chapter-1.js";
+import "./_chapter-2.js";
+import "./_chapter-3.js";
+AOS.init();
