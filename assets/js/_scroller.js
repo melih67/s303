83acb6a0c782879c84b60773b1f2dd52b2e5ae55
Mael Garrigues <maelgarrigues@gmx.com>
2024-02-04 21:18:21 +0100

    accessible nav bar

--- assets/js/_scroller.js
+++ assets/js/_scroller.js
@@ -1,24 +1,27 @@
-/* document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.chapter');
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector('.side-nav')
   const navLinks = document.querySelectorAll('.side-nav__link');
+  const story = document.getElementById('histoire');
+  const chapters = document.querySelectorAll('.chapter');
+  const options = {
+    root: story,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
 
-  window.addEventListener('scroll', () => {
-    let currentSection = "";
-
+  const observer = new IntersectionObserver((sections) => {
     sections.forEach(section => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.clientHeight;
-      if (pageYOffset >= sectionTop - sectionHeight / 3) {
-        currentSection = section.getAttribute('id');
+      if (section.isIntersecting) {
+        const id = '#' + section.target.id;
+        history.replaceState(null, '', window.location.href.split('#')[0] + id);
+        for (const link of navLinks) {
+          link.ariaCurrent = link.getAttribute('href') === id ? 'step' : null;
+        }
       }
     });
+  }, options);
 
-    navLinks.forEach(link => {
-      link.classList.remove('active');
-      if (link.href.split('#')[1] === currentSection) {
-        link.classList.add('active');
-      }
-    });
-  });
+  for (const section of chapters) {
+    observer.observe(section);
+  }
 });
- */
