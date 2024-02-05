65bcbc8205bf9019172b4f7812c304e06a28ed37
AkifiNail <nail.akifi1234@gmail.com>
2024-02-05 23:54:04 +0100

    S1A2 mise en place des graphiques animés

--- assets/js/anim_gif.js
+++ assets/js/anim_gif.js
@@ -5,3 +5,11 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: "anim/s1a1/lampe.json",
 });
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("s1a2"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "anim/s1a2/s1a2.json",
+});
